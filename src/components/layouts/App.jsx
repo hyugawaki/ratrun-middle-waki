0f01270eed2de12f09649480c9fd69e74ea1ce93
hyugawaki <hyugafrenchstudy@gmail.com>
2023-05-30 15:36:54 +0200

    footerを追加しました

--- src/components/layouts/App.jsx
+++ src/components/layouts/App.jsx
@@ -1,11 +1,13 @@
 import Header from "../organisms/Header";
+import Footer from "../organisms/Footer";
 
 const App = () => {
   return (
     <div>
       <Header />
+      <Footer />
     </div>
   );
-}
+};
 
 export default App;
