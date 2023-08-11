eead1e97197422ff9550e77579fa41bc2636fc77
hyugawaki <hyugafrenchstudy@gmail.com>
2023-08-11 12:50:37 +0900

    pngフォルダを作り、pngをまとめ、index.jsを作りました。

---
+++ src/assets/png/index.js
@@ -0,0 +1,6 @@
+import philosophy from "./philosophy-picture.png";
+import vision from "./vision-picture.png"
+import mission from "./mission-picture.png"
+import office from "./office-picture.png"
+
+export {philosophy, vision, mission, office}; 
